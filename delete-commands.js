e16a622cfc9f16c7301bbf1a124a3dff80a2a1ff
Alan Luu <alanluu4@gmail.com>
2023-06-05 18:54:57 -0700

    Update messages for deploy/delete commands scripts

--- delete-commands.js
+++ delete-commands.js
@@ -4,17 +4,27 @@ const { clientId, guildId, token } = require("./config.json");
 const rest = new REST().setToken(token);
 (async () => {
     try {
-        console.log("Started deleting all application (/) commands.");
-        await rest.put(
-            (guildId ? Routes.applicationGuildCommands(clientId, guildId)
-                : Routes.applicationCommands(clientId)),
-            {
-                body: []
-            },
-        );
-        console.log("Successfully deleted all application (/) commands.");
+        if (guildId) {
+            console.log(`Started deleting all application (/) commands from guild ${guildId}.`);
+            await rest.put(
+                Routes.applicationGuildCommands(clientId, guildId),
+                {
+                    body: []
+                },
+            );
+            console.log(`Successfully deleted all application (/) commands from guild ${guildId}.`);
+        } else {
+            console.log("Started deleting all application (/) commands.");
+            await rest.put(
+                Routes.applicationCommands(clientId),
+                {
+                    body: []
+                },
+            );
+            console.log("Successfully deleted all application (/) commands.");
+        }
     } catch (e) {
         console.error(e);
-        console.log("Failed to delete one or more application (/) commands.");
+        console.log(`Failed to delete one or more application (/) commands${guildId ? ` from guild ${guildId}` : ""}.`);
     }
 })();
--- deploy-commands.js
+++ deploy-commands.js
@@ -10,19 +10,27 @@ for (const command of commandsCollection.values()) {
 const rest = new REST().setToken(token);
 (async () => {
     try {
-        console.log(`Started deploying ${commands.length} application (/) commands.`);
-
-        const data = await rest.put(
-            (guildId ? Routes.applicationGuildCommands(clientId, guildId)
-                : Routes.applicationCommands(clientId)),
-            {
-                body: commands
-            },
-        );
-
-        console.log(`Successfully deployed ${data.length} application (/) commands.`);
+        if (guildId) {
+            console.log(`Started deploying ${commands.length} application (/) commands to guild ${guildId}.`);
+            const data = await rest.put(
+                Routes.applicationGuildCommands(clientId, guildId),
+                {
+                    body: commands
+                },
+            );
+            console.log(`Successfully deployed ${data.length} application (/) commands to guild ${guildId}.`);
+        } else {
+            console.log(`Started deploying ${commands.length} application (/) commands.`);
+            const data = await rest.put(
+                Routes.applicationCommands(clientId),
+                {
+                    body: commands
+                },
+            );
+            console.log(`Successfully deployed ${data.length} application (/) commands.`);
+        }
     } catch (e) {
         console.error(e);
-        console.log("Failed to deploy one or more application (/) commands.");
+        console.log(`Failed to deploy one or more application (/) commands${guildId ? ` to guild ${guildId}` : ""}.`);
     }
 })();
